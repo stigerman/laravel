4fa1b72cf03c7bac08f11221644cb842619352bd
stigerman <jarred.stiger@gmail.com>
2016-08-22 07:48:27 +0000

    first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', 'MainController@index');
+Route::get('/admin/product/new', 'ProductController@newProduct');
+Route::get('/admin/products', 'ProductController@index');
+Route::get('/admin/product/destroy/{id}', 'ProductController@destroy');
+Route::post('/admin/product/save', 'ProductController@add');
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+// Cart routes...
+Route::get('/addProduct/{productId}', 'CartController@addItem');
+Route::get('/removeItem/{productId}', 'CartController@removeItem');
+Route::get('/cart', 'CartController@showCart');
+ 
