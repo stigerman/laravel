3de4a079b61c3f3fef8b8cbe5370a5f730aa837e
stigerman <jarred.stiger@gmail.com>
2016-08-22 11:41:31 +0000

    fix2

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -26,7 +26,7 @@
                                             <label>${{$product->price}}</label></h3>
                                     </div>
                                 </div>
-                                <p>{{$product->description}}</p>
+                                <!--<p>{{$product->description}}</p>-->
                                 <div class="row">
                                     <div class="col-md-6 col-md-offset-3">
                                         <a href="/addProduct/{{$product->id}}" class="btn btn-success btn-product"><span class="fa fa-shopping-cart"></span> Buy</a></div>
